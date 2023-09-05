554eb93668536a1ce4d0e8b65cde385bf4fe5faf
AkosiDK <darrylkayeabellasanga@gmail.com>
2023-09-05 17:21:11 +0800

    add projects route

---
+++ src/pages/projects/index.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const Projects = () => {
+    return (
+        <section>
+            <div>
+                <h1 className="text-center">Projects</h1><br/>
+            </div>
+        </section>
+    )
+}
+
+export default Projects
