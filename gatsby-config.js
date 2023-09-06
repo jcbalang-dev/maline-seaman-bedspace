46a098cb3720538d6bdba8cc3fb298e912672eaf
John Christian Balang <johnchristianbalang@Johns-MacBook-Pro.local>
2023-09-07 04:46:40 +0800

    force install bootstrap-react & bootstrap
    - force update dependencies
    - in case other PR installation with bootstrap,
    must resolve conflict before merging

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,26 +4,46 @@
 module.exports = {
   siteMetadata: {
     title: `Maline's Bedspace Seaman's Dormitory and Transient Inn`,
-    siteUrl: `https://malineseamanbedspace.com.ph`
+    description: `Seaman's Haven`,
+    siteUrl: `https://malineseamanbedspace.com.ph`,
+    copyright: `Do Little Studio Copyright 2023`,
+    contact: `dev.jcbalang@gmail.com`
   },
-  plugins: ["gatsby-plugin-netlify-cms", {
-    resolve: 'gatsby-plugin-google-gtag',
-    options: {
-      trackingIds: [
-        "G-HR6XS55W4R" // Google Analytics / GA
-      ]
+  plugins: [
+    "gatsby-transformer-remark",
+    "gatsby-plugin-netlify-cms", 
+    {
+      resolve: 'gatsby-plugin-google-gtag',
+      options: {
+        trackingIds: [
+          "G-HR6XS55W4R" // Google Analytics / GA
+        ]
+      }
+    }, 
+    "gatsby-plugin-image", 
+    "gatsby-plugin-sitemap", 
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/icon.png"
+      }
+    }, 
+    "gatsby-plugin-sharp", 
+    "gatsby-transformer-sharp", 
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    }, 
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "projects",
+        "path": "./src/projects/"
+      }
     }
-  }, "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }]
+  ]
 };
---
+++ src/pages/projects/index.js
@@ -0,0 +1,62 @@
+import { Link, graphql } from 'gatsby';
+
+import React from 'react'
+import Layout from '../../components/Layout'
+import * as styles from '../../styles/projects.module.css'
+
+import { GatsbyImage } from 'gatsby-plugin-image'
+
+const Projects = ({ data }) => {
+  console.log(data)
+  const projects = data.projects.nodes
+  const contact = data.contact.siteMetadata.contact
+
+  return (
+    <Layout>
+      <div className={styles.portfolio}>
+        <h2>Portfolio</h2>
+        <h3>Projects & Websites I've Created</h3>
+        <div className={styles.projects}>
+          {projects.map(project => (
+            <Link to={"/projects/" + project.frontmatter.slug } key={project.id}>
+              <div>
+               <GatsbyImage image={project.frontmatter.thumb.childImageSharp.gatsbyImageData} />  
+                <h3>{ project.frontmatter.title }</h3>
+                <p>{ project.frontmatter.stack }</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+        <p>Like what you see? Email me at { contact } for a quote</p>
+      </div>
+    </Layout>
+  );
+}
+ 
+export default Projects
+
+// export page query
+export const query = graphql`
+  query ProjectsPage {
+    projects: allMarkdownRemark(sort: {frontmatter: {date: DESC}}) {
+      nodes {
+        frontmatter {
+          slug
+          stack
+          title
+          thumb {
+            childImageSharp {
+              gatsbyImageData(layout: FULL_WIDTH)
+            }
+          }
+        }
+        id
+      }
+    }
+    contact: site {
+      siteMetadata {
+        contact
+      }
+    }
+  }
+`
