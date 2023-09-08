c3fc55b864e13fc19aad94aa76dba832be95bc82
AkosiDK <darrylkayeabellasanga@gmail.com>
2023-09-08 16:58:08 +0800

    add logo

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '/src/styles/global.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '/src/styles/global.css'
+
+import { Container, Row, Col, Image } from 'react-bootstrap'
+import logo from '/src/images/logo.png'
 
 const Projects = () => {
     return (
+        <>
             <div className="bg">
-
+                <Container>
+                    <Row>
+                        <Col>
+                            <Image src={logo} alt="logo" fluid />
+                        </Col>
+                    </Row>
+                </Container>
             </div>
+        </>
     )
 }
 
-export default Projects;
+export default Projects
