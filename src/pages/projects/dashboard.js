2bb625e52a5b613bdaeecfdf59199ba5b6c6978a
AkosiDK <darrylkayeabellasanga@gmail.com>
2023-09-13 20:41:15 +0800

    add dashboard content location and search

--- src/pages/projects/dashboard.js
+++ src/pages/projects/dashboard.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Row, Col, Navbar, Nav } from 'react-bootstrap';
+import { Container, Row, Col, Navbar, Nav, Form, FormControl } from 'react-bootstrap';
 import { Icon } from '@iconify/react';
 import logo from '/src/images/logo.png';
 import userfooter from '/src/images/user.png';
@@ -14,8 +14,10 @@ const Dashboard = () => {
     return (
         <Container fluid>
             <Row className='position-relative'>
+                {/* Sidebar */}
                 <Col sm={3} md={2} className="min-vh-100 p-0 position-relative d-flex justify-content-between">
                     <Navbar className="flex-column position-relative">
+                        {/* Logo and Brand */}
                         <Navbar.Brand className='d-flex align-items-center my-3'>
                             <img
                                 src={logo}
@@ -26,9 +28,17 @@ const Dashboard = () => {
                             />
                             <span className='my-auto fw-medium lh-sm text-wrap mx-3 homelogoname'>Malyns Transient House</span>
                         </Navbar.Brand>
+                        {/* Sidebar Navigation */}
                         <Nav className="flex-column w-100">
-                            <Nav.Link href="#" className='px-5 menuitemname d-flex align-items-center'><Icon icon="lucide:home" width="21.28" height="21.28" /><span className='mx-2'>Home</span></Nav.Link>
-                            <Nav.Link href="#" className='px-5 menuitemname d-flex align-items-center'><Icon icon="lucide:layout-dashboard" width="21.28" height="21.28" /><span className='mx-2'>Dashboard </span></Nav.Link>
+                            <Nav.Link href="#" className='px-5 menuitemname d-flex align-items-center'>
+                                <Icon icon="lucide:home" width="21.28" height="21.28" />
+                                <span className='mx-2'>Home</span>
+                            </Nav.Link>
+                            <Nav.Link href="#" className='px-5 menuitemname d-flex align-items-center'>
+                                <Icon icon="lucide:layout-dashboard" width="21.28" height="21.28" />
+                                <span className='mx-2'>Dashboard</span>
+                            </Nav.Link>
+                            {/* Services Menu */}
                             <div className='servicesmenu px-5 d-flex align-items-center justify-content-between' onClick={toggleMenu}>
                                 <div className="d-flex align-items-center">
                                     <Icon icon="lucide:box" width="21.28" height="21.28" />
@@ -36,7 +46,7 @@ const Dashboard = () => {
                                 </div>
                                 <Icon icon={isMenuOpen ? 'akar-icons:chevron-up' : 'akar-icons:chevron-down'} width="16" height="16" />
                             </div>
-
+                            {/* Services Submenu (conditionally rendered) */}
                             {isMenuOpen && (
                                 <div className="servicesmenu-position">
                                     <Nav.Link href="#" className='servicessubmenu d-flex align-items-center'><span>Available Pending</span></Nav.Link>
@@ -47,14 +57,25 @@ const Dashboard = () => {
                             )}
                         </Nav>
                     </Navbar>
-
+                    {/* Sidebar Footer */}
                     <div className='footer position-absolute w-100 bottom-0 left-0'>
-                        <div className='divider position-relative'></div>
+                        <div className='divider position-relative w-75'></div>
+                        {/* Sidebar Footer Navigation */}
                         <div className='sidenav-footer py-5'>
-                            <Nav.Link href="#" className='px-5 menuitemname d-flex align-items-center'><Icon icon="lucide:message-square" width="21.28" height="21.28" /><span className='mx-2'>Chat</span></Nav.Link>
-                            <Nav.Link href="#" className='px-5 menuitemname d-flex align-items-center'><Icon icon="lucide:settings" width="21.28" height="21.28" /><span className='mx-2'>Settings</span></Nav.Link>
-                            <Nav.Link href="#" className='px-5 menuitemname d-flex align-items-center'><Icon icon="lucide:log-out" width="21.28" height="21.28" /><span className='mx-2'>Logout</span></Nav.Link>
+                            <Nav.Link href="#" className='px-5 menuitemname d-flex align-items-center'>
+                                <Icon icon="lucide:message-square" width="21.28" height="21.28" />
+                                <span className='mx-2'>Chat</span>
+                            </Nav.Link>
+                            <Nav.Link href="#" className='px-5 menuitemname d-flex align-items-center'>
+                                <Icon icon="lucide:settings" width="21.28" height="21.28" />
+                                <span className='mx-2'>Settings</span>
+                            </Nav.Link>
+                            <Nav.Link href="#" className='px-5 menuitemname d-flex align-items-center'>
+                                <Icon icon="lucide:log-out" width="21.28" height="21.28" />
+                                <span className='mx-2'>Logout</span>
+                            </Nav.Link>
                         </div>
+                        {/* User Info in Sidebar Footer */}
                         <div className='usersidenavfooter d-flex align-items-center px-5 pb-5'>
                             <div className='uservatar d-inline-block'>
                                 <img
@@ -68,7 +89,20 @@ const Dashboard = () => {
                             </div>
                         </div>
                     </div>
-
+                </Col>
+                {/* Content */}
+                <Col sm={9} md={10} className="content">
+                    <Row className='d-flex align-items-center py-4 border-bottom border-light-subtle'>
+                        <Col xs={8}>
+                            <h1 className='locationheader my-auto'>1856C Agoncillo St. Malate Manila</h1>
+                        </Col>
+                        <Col xs={4}>
+                            <Form inline className="float-right input-wrapper d-flex align-items-center w-100 border border-light-subtle">
+                                <FormControl type="text" placeholder="Search..." className="mr-sm-2 inputsearch bg-transparent border border-0 w-100"/>
+                                <Icon icon="lucide:search" width="21.28" height="21.28" id='searchicon' className='mx-2'/>
+                            </Form>
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
         </Container>
