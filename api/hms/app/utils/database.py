b10edfd1029db702824359c9d79a05af594e8458
John Christian Balang <dev.jcbalang@gmail.com>
2023-09-19 16:19:11 +0800

    include one fetch of data

--- api/hms/app/utils/database.py
+++ api/hms/app/utils/database.py
@@ -3,8 +3,9 @@ from config import Config
 from app.utils.decorators import ReconnectOnFailure
 
 class MySQLQueryExecutor:
-    def __init__(self):
+    def __init__(self, isFetchAll = True ):
         self.db = Database()
+        self.isFetchAll = isFetchAll
         self.max_retries = int(Config.DB_RECONNECT_MAX_RETRY)
 
     @ReconnectOnFailure()
@@ -13,7 +14,7 @@ class MySQLQueryExecutor:
         try:
             cursor = connection.cursor()
             cursor.execute(query, params)
-            result = cursor.fetchall()
+            result = cursor.fetchall() if self.isFetchAll else cursor.fetchone()
             cursor.close()
             return result
         finally:
