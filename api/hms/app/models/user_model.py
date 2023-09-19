1b9adc305ec6542a32cf0a43bf695fdaded98b32
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2023-09-20 06:39:28 +0800

    fix reconnection using MySQLQueryExecutor

--- api/hms/app/models/user_model.py
+++ api/hms/app/models/user_model.py
@@ -1,6 +1,4 @@
-from db import Database
-
-db = Database()
+from app.utils.database import MySQLQueryExecutor
 
 class User:
     def __init__(self, id, last_name, first_name, middle_name, user_id, email, status):
@@ -14,28 +12,24 @@ class User:
 
 class UserModel:
     def __init__(self):
-        self.db_connection = Database.get_connection()
+        self.db_exec = MySQLQueryExecutor()
 
     def get_all_users(self):
-        db_cursor = self.db_connection.cursor()
+        self.db_exec.isFetchAll = True
         
         query = "SELECT id, last_name, first_name, middle_name, user_id, email, status FROM user"
         
-        db_cursor.execute(query)
-        users = db_cursor.fetchall()
-        db_cursor.close()
+        users = self.db_exec.execute_query(query)
         
         return users 
 
     def get_user(self, user_id, password):
-        db_cursor = self.db_connection.cursor()
+        self.db_exec.isFetchAll = False
         
         query = "SELECT id, last_name, first_name, middle_name, user_id, email, status FROM user WHERE user_id = %s AND password = %s"
         
-        db_cursor.execute(query, (user_id,password))
-        result = db_cursor.fetchone()
-        db_cursor.close()
-
+        params = (user_id, password)
+        result = self.db_exec.execute_query(query, params)
         if result:
             id, last_name, first_name, middle_name, user_id, email, status = result
             
