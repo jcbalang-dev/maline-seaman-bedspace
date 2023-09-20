b91f534aa9e3ba666a208e452349f3cbb8cb2289
John Christian Balang <dev.jcbalang@gmail.com>
2023-09-20 16:11:58 +0800

    add routes for /users and /users/<string:id>

--- api/hms/routes.py
+++ api/hms/routes.py
@@ -1,5 +1,5 @@
 from flask_restful import Api
-from app.controllers.user_controller import UserAuthController
+from app.controllers.user_controller import GetAllUserController, GetUserIdController, UserAuthController
 from app.controllers.guest_controller import GetAllGuestController, GetGuestIDController
 
 class Routes:
@@ -9,5 +9,7 @@ class Routes:
     def setup_routes(self):
         # routes list
         self.api.add_resource(UserAuthController, "/login")
+        self.api.add_resource(GetAllUserController, "/users")
+        self.api.add_resource(GetUserIdController, "/user/<string:id>")
         self.api.add_resource(GetAllGuestController, "/guests")
         self.api.add_resource(GetGuestIDController, "/guest/<string:guest_id>")
