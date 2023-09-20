0156916b97b7c565896a1d1d07d350fa2d8dd286
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2023-09-20 09:57:09 +0800

    add room_bedspace routes

--- api/hms/routes.py
+++ api/hms/routes.py
@@ -2,6 +2,7 @@ from flask_restful import Api
 from app.controllers.user_controller import UserAuthController
 from app.controllers.building_controller import GetAllBuildingController, GetBuildingIDController
 from app.controllers.room_controller import GetAllRoomController, GetRoomIDController
+from app.controllers.room_bedspace_controller import GetAllRoomBedSpaceController, GetRoomBedSpaceIDController
 
 class Routes:
     def __init__(self, api):
@@ -14,3 +15,5 @@ class Routes:
         self.api.add_resource(GetBuildingIDController, "/building/<string:building_id>")
         self.api.add_resource(GetAllRoomController, "/rooms")
         self.api.add_resource(GetRoomIDController, "/room/<string:room_id>")
+        self.api.add_resource(GetAllRoomBedSpaceController, "/room_bedspaces")
+        self.api.add_resource(GetRoomBedSpaceIDController, "/room_bedspace/<string:room_bedspace_id>")
