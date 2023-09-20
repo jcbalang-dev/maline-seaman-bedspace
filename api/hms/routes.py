e11bb6c7cc8ad5a612d4622ab497ef50d3ef0c89
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2023-09-20 17:45:43 +0800

    add building routes

--- api/hms/routes.py
+++ api/hms/routes.py
@@ -1,6 +1,7 @@
 from flask_restful import Api
 from app.controllers.user_controller import UserAuthController
 from app.controllers.guest_controller import GetAllGuestController, GetGuestIDController
+from app.controllers.building_controller import GetAllBuildingController, GetBuildingIDController
 
 class Routes:
     def __init__(self, api):
@@ -11,3 +12,5 @@ class Routes:
         self.api.add_resource(UserAuthController, "/login")
         self.api.add_resource(GetAllGuestController, "/guests")
         self.api.add_resource(GetGuestIDController, "/guest/<string:guest_id>")
+        self.api.add_resource(GetAllBuildingController, "/buildings")
+        self.api.add_resource(GetBuildingIDController, "/building/<string:building_id>")
