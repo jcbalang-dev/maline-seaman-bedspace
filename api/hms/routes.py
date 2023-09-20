15a93b6fb7ce26f37aca08e99422dfe7ab373996
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2023-09-20 18:07:04 +0800

    add room routes

--- api/hms/routes.py
+++ api/hms/routes.py
@@ -1,6 +1,7 @@
 from flask_restful import Api
 from app.controllers.user_controller import UserAuthController
 from app.controllers.guest_controller import GetAllGuestController, GetGuestIDController
+from app.controllers.room_controller import GetAllRoomController, GetRoomIDController
 
 class Routes:
     def __init__(self, api):
@@ -11,3 +12,5 @@ class Routes:
         self.api.add_resource(UserAuthController, "/login")
         self.api.add_resource(GetAllGuestController, "/guests")
         self.api.add_resource(GetGuestIDController, "/guest/<string:guest_id>")
+        self.api.add_resource(GetAllRoomController, "/rooms")
+        self.api.add_resource(GetRoomIDController, "/room/<string:room_id>")
