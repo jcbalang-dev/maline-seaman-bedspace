a42369a76a54e16f461fcee02903cf366596bc12
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2023-09-21 14:41:17 +0800

    fix bug returning JSON sorted alphabetically

--- api/hms/app.py
+++ api/hms/app.py
@@ -8,6 +8,9 @@ from routes import Routes
 # Initialize the Flask app
 app = Flask(__name__)
 
+# Disable JSON sorting alphabetically globally
+app.json.sort_keys = False
+
 # Load environment variables from .env
 env = dotenv_values()
 
