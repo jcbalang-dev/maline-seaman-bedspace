7fcef7795a9ed28fa399e9b401854440739b3bf3
John Christian Balang <dev.jcbalang@gmail.com>
2023-09-29 02:20:11 +0800

    change styling source from global to projects

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import '/src/styles/global.css'
+import * as styles from '../../styles/projects.module.css'
 
 import { Container, Row, Col, Image, Form, Button, Card } from 'react-bootstrap'
 import logo from '/src/images/logo.png'
 
 const Projects = () => {
 
-
     return (
         <>
-            <div className="bg">
-                <Container className='pt-5'>
-
+            <div className={styles.bg}>
+                <Container className={`${styles.bg} pt-5`}>
                     <Row>
                         <Col xs={4} md={3} lg={2}>
                             <a href="#!">
@@ -23,19 +21,19 @@ const Projects = () => {
 
                     <Row className="d-flex justify-content-center align-items-center">
                         <Col xs={12} lg={8}>
-                            <Card className="cardform rounded-5 px-5 pb-3">
+                            <Card className={`${styles.cardform} rounded-5 px-5 pb-3`}>
                                 <Card.Body>
                                     <div className="mb-3">
-                                        <p className="fw-bold mb-2 loginheader">Login</p>
-                                        <p className=" mb-5 loginsubheader">Login to access your Golobe account</p>
+                                        <p className={`fw-bold mb-2 ${styles.loginheader}`}>Login</p>
+                                        <p className={`mb-5 ${styles.loginsubheader}`}>Login to access your Golobe account</p>
                                         <Form>
                                             <Form.Group className="mb-3" controlId="formBasicEmail">
-                                                <Form.Label className='formlabellogin'>Email address</Form.Label>
+                                                <Form.Label className={styles.formlabellogin}>Email address</Form.Label>
                                                 <Form.Control type="email" placeholder="contactdolittle@gmail.com" className='formtxtboxlogin' />
                                             </Form.Group>
 
                                             <Form.Group className="mb-3" controlId="formBasicPassword">
-                                                <Form.Label className='formlabellogin'>Password</Form.Label>
+                                                <Form.Label className={styles.formlabellogin}>Password</Form.Label>
                                                 <Form.Control type="password" placeholder="•••••••••••••••••••••••••" className='formtxtboxlogin' />
                                             </Form.Group>
 
@@ -46,7 +44,7 @@ const Projects = () => {
                                                 </Form.Text>
                                             </Form.Group>
 
-                                            <Button type="submit" className='mt-5 w-100 fw-semibold formbtnsubmit'>
+                                            <Button type="submit" className={`mt-5 w-100 fw-semibold ${styles.formbtnsubmit}`}>
                                                 Submit
                                             </Button>
                                         </Form>
@@ -55,7 +53,6 @@ const Projects = () => {
                             </Card>
                         </Col>
                     </Row>
-                    
                 </Container>
             </div>
         </>
