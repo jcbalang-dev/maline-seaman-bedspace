45391354e7c91f52151da97a40ef9bc2acfbcc23
John Christian Balang <dev.jcbalang@gmail.com>
2023-10-13 21:09:06 +0800

    prepare room for abstraction

--- api/hms/app/views/room_view.py
+++ api/hms/app/views/room_view.py
@@ -4,20 +4,20 @@ class RoomView:
     @staticmethod
     def serialize_room(room):
         return {
-            'id': room.id,
-            'building_id': room.building_id,
-            'number': room.number,
-            'code': room.code,
-            'tag': room.tag,
-            'slug': room.slug,
-            'rate': float(room.rate),
-            'bed_capacity': room.bed_capacity,
-            'availability': room.availability,
-            'description': room.description,
-            'added_by': room.added_by,
-            'added_date': room.added_date.strftime("%Y-%m-%d %H:%M:%S"),
-            'updated_by': room.updated_by,
-            'updated_date': room.updated_date.strftime("%Y-%m-%d %H:%M:%S"),
+            "id" : room.id ,
+            "building_id" : room.building_id ,
+            "number" : room.number ,
+            "code" : room.code ,
+            "tag" : room.tag ,
+            "slug" : room.slug ,
+            "rate" : room.rate ,
+            "bed_capacity" : room.bed_capacity ,
+            "availability" : room.availability ,
+            "description" : room.description ,
+            "added_by" : room.added_by ,
+            "added_date" : room.added_date ,
+            "updated_by" : room.updated_by ,
+            "updated_date" : room.updated_date
         }
 
     @staticmethod
@@ -28,22 +28,21 @@ class RoomView:
     def render_rooms(rooms):
         result = []
         for room in rooms:
-            room_dict = {
-                'id': room[0],
-                'building_id': room[1],
-                'number': room[2],
-                'code': room[3],
-                'tag': room[4],
-                'slug': room[5],
-                'rate': float(room[6]),  # Convert Decimal to float
-                'bed_capacity': room[7],
-                'availability': room[8],
-                'description': room[9],
-                'added_by': room[10],
-                'added_date': room[11].strftime("%Y-%m-%d %H:%M:%S"),
-                'updated_by': room[12],
-                'updated_date': room[13].strftime("%Y-%m-%d %H:%M:%S"),
+            dict = {
+                "id" : room.id ,
+                "building_id" : room.building_id ,
+                "number" : room.number ,
+                "code" : room.code ,
+                "tag" : room.tag ,
+                "slug" : room.slug ,
+                "rate" : room.rate ,
+                "bed_capacity" : room.bed_capacity ,
+                "availability" : room.availability ,
+                "description" : room.description ,
+                "added_by" : room.added_by ,
+                "added_date" : room.added_date ,
+                "updated_by" : room.updated_by ,
+                "updated_date" : room.updated_date
             }
-            result.append(room_dict)
-
-        return {'rooms': result}
+            result.append(dict)
+        return jsonify( { 'rooms' : result } )
