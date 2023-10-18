cb320ebc7b67749e72e194a69c07da50f8b9aab5
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2023-10-18 18:02:39 +0800

    implement table union for building and user

--- api/hms/app/views/room_rate_view.py
+++ api/hms/app/views/room_rate_view.py
@@ -1,47 +1,119 @@
 from flask import jsonify
+from app.models.building_model import BuildingModel
+from app.models.user_model import UserModel
+
+building_model = BuildingModel()
+user_model = UserModel()
 
 class RoomRateView:
     @staticmethod
-    def serialize_room_rate(room_rate):
-        return {
-            'id': room_rate.id,
-            'building_id': room_rate.building_id,
-            'number': room_rate.number,
-            'code': room_rate.code,
-            'tag': room_rate.tag,
-            'slug': room_rate.slug,
-            'description': room_rate.description,
-            'price': float(room_rate.price),
-            'status': room_rate.status,
-            'added_by': room_rate.added_by,
-            'added_date': room_rate.added_date.strftime("%Y-%m-%d %H:%M:%S"),
-            'updated_by': room_rate.updated_by,
-            'updated_date': room_rate.updated_date.strftime("%Y-%m-%d %H:%M:%S"),
+    def serialize_room(room_rate):
+        
+        building = building_model.get(room_rate.building_id)
+        building_dict = {
+            'building_id' : building.id ,
+            'building_code' : building.code ,
+            'building_tag' : building.tag ,
+            'building_slug' : building.slug ,
+            'building_name' : building.name ,
+            'building_address' : building.address ,
+            'building_description' : building.description
         }
-
+        
+        added_by = user_model.get(room_rate.added_by)
+        added_by_dict = {
+            'id' : added_by.id,
+            'last_name' : added_by.last_name,
+            'first_name' : added_by.first_name,
+            'middle_name' : added_by.middle_name,
+            'user_id' : added_by.user_id,
+            'email' : added_by.email,
+            'status' : added_by.status
+        }
+        
+        updated_by = user_model.get(room_rate.updated_by)
+        updated_by_dict = {
+            'id' : updated_by.id,
+            'last_name' : updated_by.last_name,
+            'first_name' : updated_by.first_name,
+            'middle_name' : updated_by.middle_name,
+            'user_id' : updated_by.user_id,
+            'email' : updated_by.email,
+            'status' : updated_by.status
+        }
+        
+        return {
+             "id" : room_rate.id,
+             "building_id" : building_dict,
+             "number" : room_rate.number,
+             "code" : room_rate.code,
+             "tag" : room_rate.tag,
+             "slug" : room_rate.slug,
+             "description" : room_rate.description,
+             "price": room_rate.price,
+             "status" : room_rate.status,
+             "added_by" : added_by_dict ,
+             "added_date" : room_rate.added_date ,
+             "updated_by" : updated_by_dict ,
+             "updated_date": room_rate.updated_date
+        }
+        
     @staticmethod
     def render_room_rate(room_rate):
-        return jsonify(RoomRateView.serialize_room_rate(room_rate))
-
+        return jsonify(RoomRateView.serialize_room(room_rate))
+    
     @staticmethod
     def render_room_rates(room_rates):
         result = []
         for room_rate in room_rates:
-            room_rate_dict = {
-                'id': room_rate[0],
-                'building_id': room_rate[1],
-                'number': room_rate[2],
-                'code': room_rate[3],
-                'tag': room_rate[4],
-                'slug': room_rate[5],
-                'description': room_rate[6],
-                'price': float(room_rate[7]),
-                'status': room_rate[8],
-                'added_by': room_rate[9],
-                'added_date': room_rate[10].strftime("%Y-%m-%d %H:%M:%S"),
-                'updated_by': room_rate[11],
-                'updated_date': room_rate[12].strftime("%Y-%m-%d %H:%M:%S"),
+            building = building_model.get(room_rate.building_id)
+            building_dict = {
+                'building_id' : building.id ,
+                'building_code' : building.code ,
+                'building_tag' : building.tag ,
+                'building_slug' : building.slug ,
+                'building_name' : building.name ,
+                'building_address' : building.address ,
+                'building_description' : building.description
             }
-            result.append(room_rate_dict)
+            
+            added_by = user_model.get(room_rate.added_by)
+            added_by_dict = {
+                'id' : added_by.id,
+                'last_name' : added_by.last_name,
+                'first_name' : added_by.first_name,
+                'middle_name' : added_by.middle_name,
+                'user_id' : added_by.user_id,
+                'email' : added_by.email,
+                'status' : added_by.status,
+            }
 
-        return {'room_rates': result}
+            updated_by = user_model.get(room_rate.updated_by)
+            updated_by_dict = {
+                'id' : updated_by.id,
+                'last_name' : updated_by.last_name,
+                'first_name' : updated_by.first_name,
+                'middle_name' : updated_by.middle_name,
+                'user_id' : updated_by.user_id,
+                'email' : updated_by.email,
+                'status' : updated_by.status,
+            }
+            
+            dict = {
+                "id" : room_rate.id,
+                "building_id" : building_dict,
+                "number" : room_rate.number,
+                "code" : room_rate.code,
+                "tag" : room_rate.tag,
+                "slug" : room_rate.slug,
+                "description" : room_rate.description,
+                "price": room_rate.price,
+                "status" : room_rate.status,
+                "added_by" : added_by_dict ,
+                "added_date" : room_rate.added_date ,
+                "updated_by" : updated_by_dict,
+                "updated_date": room_rate.updated_date
+            }
+            result.append(dict)
+        return jsonify({'rooms' : result})
+        
