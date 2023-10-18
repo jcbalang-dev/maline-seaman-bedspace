debaf4d7db9c01ff9df046b7a267cd641a36b87f
John Christian Balang <dev.jcbalang@gmail.com>
2023-10-18 21:34:15 +0800

    recreate with Layout implementation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,66 +1,17 @@
-import React, { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Navbar, Nav, Container} from 'react-bootstrap'
-import logo from '../images/msdwebsite/logo.png'
-import bgmain from '../images/msdwebsite/bg.png'
-import { Icon } from '@iconify/react'
+import * as React from 'react'
+import Layout from '../components/Layout'
+import * as styles from '../styles/home.module.css'
 
-const Home = () => {
-  const bgStyle = {
-    backgroundImage:`url(${bgmain})`,
-    backgroundRepeat:'no-repeat',
-    backgroundPosition: 'center',
-    backgroundSize: 'cover'
-  }
-
-  const [isServicesHovered, setIsServicesHovered] = useState(false);
-
-  const handleServicesMouseEnter = () => {
-    setIsServicesHovered(true);
-  };
-
-  const handleServicesMouseLeave = () => {
-    setIsServicesHovered(false);
-  };
-
+const Home = ({ data }) => {
   return (
-    <div className={bgStyle}>
-      <Container>
-        <Navbar bg="dark" variant="dark" expand="lg">
-          <Navbar.Brand href="index.js">
-            <img src={logo} alt="MSD Logo" loading="lazy" className="img-fluid logo" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarButtonsExample" aria-label="Toggle navigation">
-            <Icon icon="lucide:menu" width="24" height="24" />
-          </Navbar.Toggle>
-          <Navbar.Collapse id="navbarButtonsExample">
-            <Nav className="me-auto">
-              <Nav.Link href="index.js" className='text-white mx-4 circularmed'>Home</Nav.Link>
-              <Nav.Link href="about.js" className='text-white mx-4 circularmed'>About</Nav.Link>
-              <Nav.Link
-                href="services.js"
-                onMouseEnter={handleServicesMouseEnter}
-                onMouseLeave={handleServicesMouseLeave}
-                className='text-white mx-4 circularmed'
-              >
-                Services
-                <Icon
-                  icon={isServicesHovered ? 'lucide:chevron-up' : 'lucide:chevron-down'}
-                  width="24"
-                  height="24"
-                />
-                <Nav className="sub-nav">
-                  <Nav.Link href="single-bed-room.js" className='rounded circularmed'>Single Bedroom</Nav.Link>
-                  <Nav.Link href="double-deck-bed-room.js" className='rounded circularmed'>Double-Deck Bedroom</Nav.Link>
-                </Nav>
-              </Nav.Link>
-              <Nav.Link href="contactus.js" className='text-white mx-4 circularmed'>Contact Us</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </Container>
-    </div>
-  );
-};
+    <Layout>
+      <section>
+        <div>
+          <h2>Home</h2>
+        </div>
+      </section>
+    </Layout>
+  )
+}
 
-export default Home;
+export default Home
