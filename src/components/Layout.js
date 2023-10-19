2f73481b30fbc14867b9f305c38dd8849561e560
John Christian Balang <dev.jcbalang@gmail.com>
2023-10-19 11:26:50 +0800

    implement Hero component  into Layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,13 @@
 import * as React from 'react'
 import Footer from './Footer'
 import Header from './Header'
+import Hero from './Hero'
 
 export default function Layout({children}){
     return (
         <div className="layout">
             <Header />
+            <Hero />
             <div className='content'>
                 { children }
             </div>
