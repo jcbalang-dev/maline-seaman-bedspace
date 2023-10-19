8c85437e5d9d9c0ef9967d2dca44f1e7f7c6b107
John Christian Balang <dev.jcbalang@gmail.com>
2023-10-20 02:10:52 +0800

    add services route page

---
+++ src/pages/services/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../../components/Layout'
+
+const Services = ({ data }) => {
+    return (
+        <Layout>
+            <section>
+                Services
+            </section>
+        </Layout>
+    )
+}
+
+export default Services
