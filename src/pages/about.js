f346679fb4c0c57461189c806518623a703ee2ff
John Christian Balang <dev.jcbalang@gmail.com>
2023-10-20 01:41:36 +0800

    fix styling of all content

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,31 +1,31 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
 import { StaticImage } from 'gatsby-plugin-image'
+import * as styles from '../styles/about.module.css'
 
 const About = ({ data }) => {
     return (
         <Layout>
-            <section class="hero2 px-5">
-                <div class="container">
-                    <div class="row d-flex justify-content-center align-items-center">
-                        <div class="col-md-6">
-                            <div class="row hero2sub1" data-aos="fade-up" data-aos-duration="1000">
-                                <div class="col-2 lines rounded-pill px-1"></div>
-                                <div class="col-6 py-2">
+            <section className={`${styles.hero2} px-5`}>
+                <div className="container">
+                    <div className="row d-flex justify-content-center align-items-center">
+                        <div className="col-md-6">
+                            <div className={`row ${styles.hero2sub1}`} data-aos="fade-up" data-aos-duration="1000">
+                                <div className={`col-2 ${styles.lines} rounded-pill px-1`}></div>
+                                <div className="col-6 py-2">
                                     <h1>About Us</h1>
                                 </div>
                             </div>
-                            <div class="row hero2sub2" data-aos="fade-up" data-aos-duration="1000">
-                                <div class="col-12">
-                                    <h2 class="josefinsitalic">A premier hospitality establishment that caters specifically to
+                            <div className={`row ${styles.hero2sub2}`} data-aos="fade-up" data-aos-duration="1000">
+                                <div className="col-12">
+                                    <h2 className="josefinsitalic">A premier hospitality establishment that caters specifically to
                                         the needs of seafarers seeking
                                         comfortable and convenient accommodations during their time ashore.
                                     </h2>
                                 </div>
                             </div>
                         </div>
-
-                        <div class="col-md-6" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
+                        <div className="col-md-6" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
                             <div>
                                 <StaticImage 
                                     src='../images/website/aboutus1.png'
@@ -35,7 +35,6 @@ const About = ({ data }) => {
                             </div>
                         </div>
                     </div>
-
                 </div>
             </section>
         </Layout>
