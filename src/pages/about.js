e5ba830a771b002a343f6906e93d7d611990d2d6
John Christian Balang <dev.jcbalang@gmail.com>
2023-10-20 01:24:11 +0800

    add About page route

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../components/Layout'
+
+const About = ({ data }) => {
+    return (
+        <Layout>
+            <section>
+                <span>About Page</span>
+            </section>
+        </Layout>
+    )
+}
+
+export default About
