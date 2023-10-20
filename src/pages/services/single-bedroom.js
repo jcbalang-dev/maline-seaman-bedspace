8f44d6c7dd4d60f4522717ed9f0590bc44d63be7
John Christian Balang <dev.jcbalang@gmail.com>
2023-10-21 00:38:00 +0800

    create services/single-bedroom route page

---
+++ src/pages/services/single-bedroom.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../../components/Layout'
+
+const SingleBedroom = ({ data }) => {
+    return (
+        <Layout>
+            <section>
+                <h1>Single Bed</h1>
+            </section>
+        </Layout>
+    )
+}
+
+export default SingleBedroom
