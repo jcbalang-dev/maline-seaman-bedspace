dc52d2c844fc0966568af6949829edd74477266d
John Christian Balang <dev.jcbalang@gmail.com>
2023-10-21 00:39:31 +0800

    create services/double-deck-bedroom route page

---
+++ src/pages/services/double-deck-bedroom.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../../components/Layout'
+
+const DoubleDeckBedroom = ({ data }) => {
+    return (
+        <Layout>
+            <section>
+                <h1>Double Deck Bed</h1>
+            </section>
+        </Layout>
+    )
+}
+
+export default DoubleDeckBedroom
