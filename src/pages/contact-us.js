0253ae75eadc6cae5035eb410a28c567c3ae4921
John Christian Balang <dev.jcbalang@gmail.com>
2023-10-22 01:33:55 +0800

    add boilerplate for /contact-us route pages

---
+++ src/pages/contact-us.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import SimpleLayout from '../components/SimpleLayout'
+
+const ContactUsPage = ({ data }) => {
+    return (
+        <SimpleLayout>
+            <region>
+                <h1>Contact Us</h1>
+            </region>
+        </SimpleLayout>
+    )
+}
+
+export default ContactUsPage
