8c1460f743c56753b488b1c6f6cce16dda296154
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2023-12-10 07:26:14 +0800

    forgot to add prettier format for readability

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,20 +1,27 @@
-import * as React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
-import * as styles from '../styles/hero.module.css'
+import * as React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import * as styles from "../styles/hero.module.css";
 
 const Hero = () => {
-    return (
-        <Container>
-            <Row>
-                <Col md={5}>
-                    <div className={styles.hero1} data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
-                        <p className="josefinsbold">Navigating Comfort:</p>
-                        <span className="josefinslight">Maline's Bedspace Seaman's Dormitory & Transient Inn</span>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <Row>
+        <Col md={5}>
+          <div
+            className={styles.hero1}
+            data-aos="fade-right"
+            data-aos-offset="300"
+            data-aos-easing="ease-in-sine"
+          >
+            <p className="josefinsbold">Navigating Comfort:</p>
+            <span className="josefinslight">
+              Maline's Bedspace Seaman's Dormitory & Transient Inn
+            </span>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default Hero
+export default Hero;
