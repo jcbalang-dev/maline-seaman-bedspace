4d5a8667cd925a842acaca5530f60b1b2a110fe9
John Christian Balang <dev.jcbalang@gmail.com>
2023-12-16 23:20:34 +0800

    include bootstrap css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 import { React, useEffect } from 'react'
+
+// aos
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+
+// bootstrap css
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
