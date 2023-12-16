a19d72c63cf529c7fb34fcb58313ac76cf920609
John Christian Balang <dev.jcbalang@gmail.com>
2023-12-17 01:46:58 +0800

    include style to application

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,6 +8,9 @@ import "aos/dist/aos.css";
 // bootstrap css
 import "bootstrap/dist/css/bootstrap.min.css";
 
+// mdb ui kit
+import "mdb-ui-kit/css/mdb.min.css";
+
 export default function App({ Component, pageProps }) {
   useEffect(() => {
     AOS.init({
