e89a1035068c417f23a5cfbd2d0b2db61e3a86a2
John Christian Balang <dev.jcbalang@gmail.com>
2023-12-17 01:14:23 +0800

    prettier code

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,9 @@
-import '../styles/globals.css'
-import { React, useEffect } from 'react'
+import "../styles/globals.css";
+import { React, useEffect } from "react";
 
 // aos
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 // bootstrap css
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -11,9 +11,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function App({ Component, pageProps }) {
   useEffect(() => {
     AOS.init({
-         duration: 800,
-         once: false,
-       })
- }, [])
-  return <Component {...pageProps} />
+      duration: 800,
+      once: false,
+    });
+  }, []);
+  return <Component {...pageProps} />;
 }
