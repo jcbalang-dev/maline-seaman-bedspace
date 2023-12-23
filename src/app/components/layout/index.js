b05d9d0de5921f303f310724d15860d0409a7bd1
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2023-12-23 13:32:02 +0800

    add head to use it into pageTitle

--- src/app/components/layout/index.js
+++ src/app/components/layout/index.js
@@ -1,15 +1,21 @@
 import * as React from "react";
 // import "mdb-ui-kit/css/mdb.min.css";
 // import "mdb-ui-kit/js/mdb.min.js";
+import Head from "next/head";
 import Footer from "../footer";
 import Header from "../header";
 
-export default function Layout({ children }) {
+export default function Layout({ children, pageTitle }) {
   return (
-    <div className="layout">
-      <Header />
-      <div className="content">{children}</div>
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+      </Head>
+      <div className="layout">
+        <Header />
+        <div className="content">{children}</div>
+        <Footer />
+      </div>
+    </>
   );
 }
