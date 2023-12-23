fb4d013fd70571884414ae95951de2a3c847da2e
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2023-12-23 13:51:49 +0800

    add head to use pageTitle

--- src/app/components/simple-layout/index.js
+++ src/app/components/simple-layout/index.js
@@ -1,15 +1,21 @@
 import * as React from "react";
 import "mdb-ui-kit/css/mdb.min.css";
+import Head from "next/head";
 
 import Footer from "../footer";
 import NavigationMenu from "../navigation-menu";
 
-export default function SimpleLayout({ children }) {
+export default function SimpleLayout({ children, pageTitle }) {
   return (
-    <div className="layout">
-      <NavigationMenu />
-      <div className="content">{children}</div>
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+      </Head>
+      <div className="layout">
+        <NavigationMenu />
+        <div className="content">{children}</div>
+        <Footer />
+      </div>
+    </>
   );
 }
