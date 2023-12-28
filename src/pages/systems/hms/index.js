1be009cffbf0be2417561ee831cc12765528eee7
John Christian Balang <dev.jcbalang@gmail.com>
2023-12-28 22:16:24 +0800

    create hms route

---
+++ src/pages/systems/hms/index.js
@@ -0,0 +1,12 @@
+const { Button } = require("antd");
+
+const HMS = () => {
+  return (
+    <div>
+      <h1>Hotel Management System</h1>
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
+};
+
+export default HMS;
