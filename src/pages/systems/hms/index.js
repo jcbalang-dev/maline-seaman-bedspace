131ee81e3284f8c4d25614eaa5bdf4e3acc6b23b
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2023-12-29 10:20:23 +0800

    change title page and add favicon

--- src/pages/systems/hms/index.js
+++ src/pages/systems/hms/index.js
@@ -1,15 +1,14 @@
+import Favicon from "../../../app/components/favicon/Favicon";
 import HMSLayout from "../../../app/components/systems/hms/index";
 
-const { Button } = require("antd");
-
 const HMS = () => {
-  const titleHome = "MSD - Hotel Management System";
+  const titleHome = "Hotel Management System";
 
   return (
-    <HMSLayout pageTitle={titleHome}>
-      <h1>MSD - Hotel Management System</h1>
-      <Button type="primary">Primary Button</Button>
-    </HMSLayout>
+    <>
+      <HMSLayout pageTitle={titleHome}></HMSLayout>
+      <Favicon />
+    </>
   );
 };
 
