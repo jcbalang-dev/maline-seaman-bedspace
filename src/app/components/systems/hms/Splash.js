503a1fd7daf600c307e9344b87e455504f9d7ad3
John Christian Balang <dev.jcbalang@gmail.com>
2023-12-29 23:46:24 +0800

    implement Logo component
    - replace hardcoded image src of logo

--- src/app/components/systems/hms/Splash.js
+++ src/app/components/systems/hms/Splash.js
@@ -1,15 +1,13 @@
 import React from "react";
 import * as styles from "../../../../styles/splash.module.css";
 import Image from "next/image";
+import Logo from "../../Logo";
 
 const Splash = () => {
   return (
     <>
       <div className={styles.splashContainer} />
-      <Image
-        src={"/images/website/logo/logo.png"}
-        className={styles.splashLogo}
-      />
+      <Logo className={styles.splashLogo} />
     </>
   );
 };
