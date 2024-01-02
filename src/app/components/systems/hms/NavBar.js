04ca50e34ee94d76912b7707705f20c387fa2b81
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2024-01-02 19:50:08 +0800

    add navbar component

---
+++ src/app/components/systems/hms/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavbarHeader from "./NavBarHeader";
+import NavbarFilter from "./NavBarFilter";
+
+const NavBar = () => {
+  return (
+    <>
+      <NavbarHeader />
+      <NavbarFilter />
+    </>
+  );
+};
+
+export default NavBar;
