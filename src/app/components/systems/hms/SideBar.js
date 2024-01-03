b7d593421e6786e4ce6921c8ef79a2bc5c1e3494
John Christian Balang <dev.jcbalang@gmail.com>
2024-01-04 00:36:06 +0800

    fix Side Bar components

--- src/app/components/systems/hms/SideBar.js
+++ src/app/components/systems/hms/SideBar.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+
 import AppTitleBar from "./AppTitleBar";
-import SideBarNavigation from "./SideBarNavigation";
 import UserProfile from "./UserProfile";
+import SidebarNavigation from "./SideBarNavigation";
+
+const { Layout } = require("antd");
+const { Sider } = Layout;
 
 const SideBar = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
-    <>
+    <Sider
+      collapsible
+      collapsed={collapsed}
+      onCollapse={(value) => setCollapsed(value)}
+      width="15%"
+    >
       <AppTitleBar />
-      <SideBarNavigation />
+      <SidebarNavigation />
       <UserProfile />
-    </>
+    </Sider>
   );
 };
 
