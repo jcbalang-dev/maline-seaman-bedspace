49cdac53647129d18f2461f30cf5507d46131ffe
John Christian Balang <dev.jcbalang@gmail.com>
2024-01-04 00:44:38 +0800

    fix sidebar on hms layout
    - need to fix all components related with this except for sidebar

--- src/app/components/systems/hms/Layout.js
+++ src/app/components/systems/hms/Layout.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import * as styles from "../../../../styles/layout.module.css";
+import SideBar from "./SideBar";
 
 const { Layout, Flex } = require("antd");
-const { Header, Footer, Sider, Content } = Layout;
+const { Header, Footer, Content } = Layout;
 
-const SystemHMSLayout = () => (
-  <Flex gap="middle" wrap="wrap">
-    <Layout className={styles.layoutStyle}>
-      <Sider
-        width="15%"
-        className={styles.siderStyle}
-        style={{ background: "#f7f7f7" }}
-      ></Sider>
-      <Layout>
-        <Header className={styles.headerStyle}></Header>
-        <Content className={styles.contentStyle}></Content>
-        <Footer className={styles.footerStyle}></Footer>
+const HMSLayout = () => {
+  return (
+    <Flex gap="middle" wrap="wrap">
+      <Layout className={styles.layoutStyle}>
+        <SideBar />
+        <Layout>
+          <Header className={styles.headerStyle}></Header>
+          <Content className={styles.contentStyle}></Content>
+          <Footer className={styles.footerStyle}></Footer>
+        </Layout>
       </Layout>
-    </Layout>
-  </Flex>
-);
-export default SystemHMSLayout;
+    </Flex>
+  );
+};
+
+export default HMSLayout;
