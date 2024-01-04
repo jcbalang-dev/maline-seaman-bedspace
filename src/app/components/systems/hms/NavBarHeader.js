4ffd5e35f8412d0df3d889a576846c692f5d2bb3
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2024-01-04 15:22:08 +0800

    refactor to the default ant design component

--- src/app/components/systems/hms/NavBarHeader.js
+++ src/app/components/systems/hms/NavBarHeader.js
@@ -1,22 +1,43 @@
 import React from "react";
-const { Row, Col, Input, Divider } = require("antd");
-import * as styles from "../../../../styles/navbar-header.module.css";
+const { Input, Space } = require("antd");
 
 const { Search } = Input;
 
-const NavbarHeader = () => {
-  return (
-    <>
-      <Row className={styles.navbarContainer}>
-        <Col className={styles.navbarHeader}>
-          1856C Agoncillio St. Malate Manila
-        </Col>
-        <Col>
-          <Search placeholder="Search..." style={{ width: 300 }} />
-        </Col>
-      </Row>
-    </>
-  );
-};
+const onSearch = (value, _e, info) => console.log(info?.source, value);
 
-export default NavbarHeader;
+const NavBarHeader = () => (
+  <Space
+    direction="horizontal"
+    style={{
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "flex-end",
+      width: "100%",
+      fontWeight: 600,
+      fontSize: 20,
+      paddingTop: 11,
+      marginLeft: 10,
+    }}
+  >
+    <p
+      style={{
+        marginBottom: 0,
+        order: 1,
+      }}
+    >
+      1856C Agoncillio St. Malate Manila
+    </p>
+    <Search
+      placeholder="Search..."
+      onSearch={onSearch}
+      style={{
+        width: 300,
+        order: 2,
+        paddingTop: 11,
+        marginRight: 20,
+      }}
+    />
+  </Space>
+);
+
+export default NavBarHeader;
