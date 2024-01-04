2b8db4da56fc2a88caac49a9fdec90fa5fc33a61
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2024-01-04 10:05:00 +0800

    change font color into white

--- src/app/components/systems/hms/AppTitleBar.js
+++ src/app/components/systems/hms/AppTitleBar.js
@@ -9,17 +9,12 @@ const AppTitleBar = () => {
     paddingTop: "10px",
   };
 
-  const appTitleBar = {
-    position: "relative",
-    left: "88px",
-    bottom: "20px",
-    fontWeight: "600",
-  };
-
   return (
     <div className="demo-logo">
       <img src="/images/website/logo/logo.png" alt="Logo" style={logoStyle} />
-      <span style={{ fontWeight: 600 }}>Malines Seaman's Dormitory</span>
+      <span style={{ fontWeight: 600, color: "#fff" }}>
+        Malines Seaman's Dormitory
+      </span>
     </div>
   );
 };
