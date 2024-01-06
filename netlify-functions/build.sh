95253a9b86545e67393e65b63aa68e467e7825b7
John Christian Balang <dev.jcbalang@gmail.com>
2024-01-07 00:16:30 +0800

    Revert "removing local python virtual environment"
    This reverts commit a7747b1d60fb5ff2dc2b9857add62a5ac7174095.

--- netlify-functions/build.sh
+++ netlify-functions/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Create a Python virtual environment
+python3 -m venv .venv
+
+# Activate the virtual environment
+source .venv/bin/activate
+
 # Install dependencies
 pip install -r api/hms/requirements.txt
 
