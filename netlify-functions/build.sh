c1d68f3509b4af5a8cb27ab268ba2bc161728158
John Christian Balang <dev.jcbalang@gmail.com>
2024-01-07 00:16:37 +0800

    Revert "fix bash script"
    This reverts commit ce0494fcdaac9443a0721f29d9c3770476f07a35.

--- netlify-functions/build.sh
+++ netlify-functions/build.sh
@@ -7,22 +7,7 @@ python3 -m venv .venv
 source .venv/bin/activate
 
 # Install dependencies
-pip install -r api/hms/requirements.txt
-
-# Copy the .env.example to the .env file
-cp api/hms/.env.example .env
-
-# Setup .env values
-sed -i "s/DB_HOST=68.178.225.224/" .env
-sed -i "s/DB_USER=msd_admin/" .env
-sed -i "s/DB_PASSWORD=SeamanTrans@2023/" .env
-sed -i "s/DB_NAME=hms/" .env
-sed -i "s/API_PORT=5001/" .env
-sed -i "s/DB_POOL_NAME=msd_hms_api/" .env
-sed -i "s/DB_POOL_SIZE=5/" .env
-sed -i "s/DB_RECONNECT_MAX_RETRY=3/" .env
-sed -i "s/FLASK_JSON_SORT_KEYS=False/" .env
-sed -i "s/CORS_ALLOWED_ORIGINS=*/" .env
+pip install -r requirements.txt
 
 # Deactivate the virtual environment
 deactivate
