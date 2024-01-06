a7747b1d60fb5ff2dc2b9857add62a5ac7174095
John Christian Balang <dev.jcbalang@gmail.com>
2024-01-06 14:50:22 +0800

    removing local python virtual environment
    - expecting that netlify has its own pre-built python virtual environment

--- netlify-functions/build.sh
+++ netlify-functions/build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Create a Python virtual environment
-python3 -m venv .venv
-
-# Activate the virtual environment
-source .venv/bin/activate
-
 # Install dependencies
 pip install -r api/hms/requirements.txt
 
