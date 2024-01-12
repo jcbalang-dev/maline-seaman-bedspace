0f525fa6cb2ce8cc244fe88078604cf0658056a8
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2024-01-12 23:38:18 +0800

    follow through in ticket DLS-MSD-APP-00098
    - Draggable Card Component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
 import "../styles/globals.css";
 import { React, useEffect } from "react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 // aos
 import AOS from "aos";
@@ -11,12 +13,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // mdb ui kit
 import "mdb-ui-kit/css/mdb.min.css";
 
-export default function App({ Component, pageProps }) {
+function App({ Component, pageProps }) {
   useEffect(() => {
     AOS.init({
       duration: 800,
       once: false,
     });
   }, []);
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <DndProvider backend={HTML5Backend}>
+        <Component {...pageProps} />
+      </DndProvider>
+    </>
+  );
 }
+
+export default App;
