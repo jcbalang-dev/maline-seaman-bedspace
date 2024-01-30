4ca260da8b162ea8573de7bc36c5fd658bf3e4ed
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2024-01-30 22:40:42 +0800

    add api call that can save in the database
    - temporarily remove the validID format function
    - temporarily working out the design that can actual work with functions
    - render the checkbox state function
    - use swal and moment library

--- src/app/components/systems/hms/GuestModal.js
+++ src/app/components/systems/hms/GuestModal.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
-const { Form, Input, Select, Switch, Checkbox } = require("antd");
-import validIDFormat from "./ValidIDFormat";
+import axios from "axios";
+import Swal from "sweetalert2";
+import moment from "moment";
+import useCheckboxState from "./checkBoxState";
 import getCharacterLimit from "./CharacterLimit";
+const { Form, Input, Select, Switch, Checkbox, Button } = require("antd");
 
-const idType = ["Passport", "Drivers License", "UMID", "SSS", "PRC"];
-
-const { Option } = Select;
+const buttonOkStyle = {
+  marginLeft: "269px",
+  position: "absolute",
+  top: "80px",
+  zIndex: "1",
+};
 
 const layout = {
   labelCol: {
@@ -17,20 +23,87 @@ const layout = {
 };
 
 const GuestModal = () => {
-  const [selectedIDType, setSelectedIDType] = useState([]);
+  const [form] = Form.useForm();
   const [switchStatus, setSwitchStatus] = useState(false);
+  const [isSubmitted, setSubmitted] = useState(false);
+
+  const suffixChange = (value) => {
+    form.setFieldsValue({
+      suffix: value === "(if applicable)" ? null : value,
+    });
+  };
+
+  const validIDList = ["Passport", "Drivers License", "UMID", "SSS", "PRC"];
+
+  const checkboxIds = [
+    "passport_id",
+    "drivers_license_id",
+    "umid_id",
+    "sss_id",
+    "prc_id",
+  ];
+
+  const checkboxState = useCheckboxState(form, checkboxIds);
 
   const handleSwitchChange = (checked) => {
     setSwitchStatus(checked);
   };
 
-  const [form] = Form.useForm();
+  const handleLastNameChange = (e) => {
+    const lastName =
+      e.target.value.charAt(0).toUpperCase() +
+      e.target.value.slice(1).toLowerCase();
+    form.setFieldsValue({ last_name: lastName });
+  };
 
-  const handleCheckboxChange = (checkedValues) => {
-    const uncheckedTypes = idType.filter((id) => !checkedValues.includes(id));
-    form.resetFields(uncheckedTypes.map((id) => `${id.toLowerCase()}`));
+  const handleFirstNameChange = (e) => {
+    const firstName = e.target.value
+      .toLowerCase()
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+    form.setFieldsValue({ first_name: firstName });
+  };
 
-    setSelectedIDType(checkedValues);
+  const handleMiddleNameChange = (e) => {
+    const middleName =
+      e.target.value.charAt(0).toUpperCase() +
+      e.target.value.slice(1).toLowerCase();
+    form.setFieldsValue({ middle_name: middleName });
+  };
+
+  const guestModalOk = () => {
+    const formData = form.getFieldsValue();
+    formData.status = switchStatus ? 1 : 0;
+    formData.added_by = formData.added_date;
+    formData.added_date = formData.updated_date = moment().format(
+      "YYYY-MM-DD HH:mm:ss"
+    );
+
+    const storedUsername = sessionStorage.getItem("username");
+    if (storedUsername) {
+      if (storedUsername === "jcbalang") {
+        formData.added_by = formData.updated_by = "1";
+      } else if (storedUsername === "raymundpaulfontanilla") {
+        formData.added_by = formData.updated_by = "2";
+      }
+    }
+
+    axios
+      .post("http://127.0.0.1:5001/add_guest", formData)
+      .then(() => {
+        Swal.fire({
+          icon: "success",
+          title: "Guest Added Successfully!",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        form.resetFields();
+        setSubmitted(true);
+      })
+      .catch((error) => {
+        console.error("Error adding guest:", error);
+      });
   };
 
   return (
@@ -38,13 +111,16 @@ const GuestModal = () => {
       {...layout}
       form={form}
       name="control-hooks"
+      onValuesChange={(allValues) => {
+        onCheckboxChange(allValues["valid-ids"] || [], form);
+      }}
       style={{
         maxWidth: 600,
         borderRadius: "8px",
       }}
     >
       <Form.Item
-        name="last name"
+        name="last_name"
         label="Lastname:"
         rules={[
           {
@@ -52,11 +128,14 @@ const GuestModal = () => {
           },
         ]}
       >
-        <Input style={{ marginLeft: "5px" }} />
+        <Input
+          style={{ marginLeft: "5px" }}
+          onChange={(e) => handleLastNameChange(e)}
+        />
       </Form.Item>
 
       <Form.Item
-        name="first name"
+        name="first_name"
         label="Firstname:"
         rules={[
           {
@@ -64,71 +143,47 @@ const GuestModal = () => {
           },
         ]}
       >
-        <Input />
+        <Input onChange={(e) => handleFirstNameChange(e, "first_name")} />
       </Form.Item>
 
-      <Form.Item
-        name="middle name"
-        label="Middlename:"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Input style={{ marginLeft: "6px", width: "auto" }} />
-      </Form.Item>
-
-      <Form.Item
-        label="Suffix"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Select placeholder="(if applicable)" allowClear>
-          <Option value="Jr.">Jr.</Option>
-          <Option value="II">II</Option>
-          <Option value="III">III</Option>
-        </Select>
-      </Form.Item>
-
-      <Form.Item name="validIDCheckbox" label="Valid ID's:">
-        <Checkbox.Group
-          options={idType}
-          onChange={(checkedValues) => {
-            handleCheckboxChange(checkedValues);
-            setSelectedIDType(checkedValues);
-          }}
+      <Form.Item name="middle_name" label="Middlename:">
+        <Input
+          style={{ marginLeft: "6px", width: "auto" }}
+          onChange={(e) => handleMiddleNameChange(e)}
         />
       </Form.Item>
 
-      {idType.map((id) => (
-        <Form.Item
-          key={id}
-          name={`${id.toLowerCase()}`}
-          label={`${id}:`}
-          rules={[
-            {
-              required: true,
-            },
-          ]}
+      <Form.Item name="suffix" label="Suffix">
+        <Select
+          placeholder="(if applicable)"
+          onChange={suffixChange}
+          allowClear
         >
-          <Input
-            style={{ marginLeft: "6px", width: "auto" }}
-            maxLength={getCharacterLimit(id)}
-            disabled={!selectedIDType.includes(id)}
-            onChange={(e) => {
-              const inputVal = e.target.value;
-              const formattedVal = validIDFormat(inputVal, selectedIDType);
-              form.setFieldsValue({
-                [`${id.toLowerCase()}`]: formattedVal,
-              });
-            }}
-          />
+          <Option value="Jr.">Jr.</Option>
+          <Option value="II">II.</Option>
+          <Option value="III">III.</Option>
+        </Select>
+
+        <Form.Item>
+          {checkboxIds.map((id, index) => (
+            <React.Fragment key={id}>
+              <Form.Item>
+                <Checkbox onChange={checkboxState[`${id}Checkbox`]}>
+                  {validIDList[index]}
+                </Checkbox>
+              </Form.Item>
+              <Form.Item name={id}>
+                <Input
+                  disabled={!checkboxState[`isChecked${id}`][0] || isSubmitted}
+                  maxLength={getCharacterLimit(
+                    validIDList[index].toUpperCase()
+                  )}
+                />
+              </Form.Item>
+            </React.Fragment>
+          ))}
         </Form.Item>
-      ))}
+      </Form.Item>
 
       <Form.Item
         label="Status"
@@ -142,6 +197,11 @@ const GuestModal = () => {
           onChange={handleSwitchChange}
           style={{ backgroundColor: switchStatus ? "green" : "red" }}
         />
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button onClick={guestModalOk} type="primary" style={buttonOkStyle}>
+            OK
+          </Button>
+        </Form.Item>
       </Form.Item>
     </Form>
   );
